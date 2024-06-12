8a56e2f0c18fbb3a1208fb27860ace80975a15be
O-keita <o.keita@alustudent.com>
2024-06-12 14:02:06 +0000

    flask part done!

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """function that returns Hello HBNB!"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function that returns HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """function that returns C followed by the value of the text variable"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """function that returns Python followed by the value of the text
+    variable"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """function that returns n is a number if n is an integer"""
+    return ("{} is a number".format(n))
+
+if __name__ == "__main__":
+    """Starts a Flask web application"""
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, redirect, url_for, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """function that returns Hello HBNB!"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function that returns HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """function that returns C followed by the value of the text variable"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """function that returns Python followed by the value of the text
+    variable"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """function that returns n is a number if n is an integer"""
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """function that returns n is a number if n is an integer"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """function that returns n is a number if n is an integer"""
+    return render_template('6-number_odd_or_even.html', number=n)
+
+if __name__ == "__main__":
+    """Starts a Flask web application"""
+    app.run(host='0.0.0.0', port=5000)
